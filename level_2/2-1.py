9c704d1fe30ddc794bee1fc603486217360b2e94
Zzphyr <49877032+Zzphyr@users.noreply.github.com>
2020-02-24 15:34:05 +0000

    Create 2-1.py

---
+++ level_2/2-1.py
@@ -0,0 +1,48 @@
+### 2-1: Nombres à virgules et autres outils ###
+
+# 1) Origami
+
+
+
+# 2) Conversions de distances
+
+
+
+# 3) Comparatif de prix
+
+
+
+# 4) Moyenne des notes
+
+
+
+# A1)  Augmentation de la population
+
+
+
+# A2) Construction de maisons
+
+
+
+# B1) Soirée orageuse
+
+
+
+# B2) Augmentation des taxes
+
+
+
+# C1) Achat de livres
+
+
+
+# C2) Une belle récolte
+
+
+
+# C3) La roue de la fortune
+
+
+
+
+## end ##
